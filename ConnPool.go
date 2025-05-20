ed3868770bf54ef10b32c150b8718b763df50d4a
go-while <go-while@users.noreply.github.com>
2025-05-20 15:35:07 +0100

    fix connect with @ in provider.Username

--- ConnPool.go
+++ ConnPool.go
@@ -7,8 +7,6 @@ import (
 	"log"
 	"net"
 	"net/textproto"
-	"os"
-	"strings"
 	"sync"
 	"time"
 )
@@ -20,12 +18,13 @@ type (
 		writer *bufio.Writer
 	}
 	ProviderConns struct {
-		openConns int
-		mux       sync.Mutex
-		pool      chan *ConnItem
-		wait      []chan *ConnItem
-		rserver   string
-		insecure  bool
+		openConns  int
+		mux        sync.Mutex
+		pool       chan *ConnItem
+		wait       []chan *ConnItem
+		provider   *Provider
+		rserver    string
+		wants_auth bool
 	}
 ) // end type
 
@@ -33,7 +32,15 @@ func NewConnPool(provider *Provider) {
 	provider.mux.Lock()         // NewConnPool mutex #d028
 	defer provider.mux.Unlock() // NewConnPool mutex #d028
 
-	srvuri := ""
+	if provider.Conns != nil {
+		return
+	}
+
+	if provider.Host == "" {
+		log.Printf("ERROR connect '%s' provider.Host empty!", provider.Name)
+		return
+	}
+
 	switch provider.TCPMode {
 	case "tcp4":
 		// pass
@@ -45,106 +52,49 @@ func NewConnPool(provider *Provider) {
 		provider.TCPMode = "tcp"
 	}
 
-	if provider.SSL {
-		switch provider.TCPMode {
-		case "tcp4":
-			srvuri = "ssl4"
-		case "tcp6":
-			srvuri = "ssl6"
-		case "tcp":
-			srvuri = "ssl"
-		default:
-			srvuri = "ssl"
+	if provider.Port <= 0 {
+		if provider.SSL {
+			provider.Port = 563
+			log.Print("WARN provider.Port not set. default to :563")
+		} else {
+			provider.Port = 119
+			log.Print("WARN provider.Port not set. default to :119")
 		}
-	} else {
-		switch provider.TCPMode {
-		case "tcp4":
-			srvuri = "tcp4"
-		case "tcp6":
-			srvuri = "tcp6"
-		case "tcp":
-			srvuri = "tcp"
-		default:
-			srvuri = "tcp"
-		}
-	}
-	if provider.Username != "" && provider.Password != "" {
-		srvuri = fmt.Sprintf("%s://%s:%s@%s:%d", srvuri, provider.Username, provider.Password, provider.Host, provider.Port)
-	} else {
-		srvuri = fmt.Sprintf("%s://%s:%d", srvuri, provider.Host, provider.Port)
 	}
 
+	rserver := fmt.Sprintf("%s:%d", provider.Host, provider.Port)
+	wants_auth := (provider.Username != "" && provider.Password != "")
 	provider.Conns = &ProviderConns{
 		pool:    make(chan *ConnItem, provider.MaxConns),
-		rserver: srvuri, insecure: provider.SkipSslCheck,
+		rserver: rserver, wants_auth: wants_auth,
 	}
 
 } // end func NewConnPool
 
-func (c *ProviderConns) connect(wid int, rserver string, insecure bool, provider *Provider, retry uint32) (*ConnItem, error) {
-	wants_ssl, wants_auth, user, pass, tcp_mode := false, false, "", "", "tcp"
+func (c *ProviderConns) connect(wid int, provider *Provider, retry uint32) (*ConnItem, error) {
 
 	if retry > provider.MaxConnErrors {
 		return nil, fmt.Errorf("ERROR connect MaxConnErrors '%s'", provider.Name)
 	}
-	if strings.HasPrefix(rserver, "tcp://") {
-		rserver = strings.Replace(rserver, "tcp://", "", 1)
-	} else if strings.HasPrefix(rserver, "tcp4://") {
-		tcp_mode = "tcp4"
-		rserver = strings.Replace(rserver, "tcp4://", "", 1)
-	} else if strings.HasPrefix(rserver, "tcp6://") {
-		tcp_mode = "tcp6"
-		rserver = strings.Replace(rserver, "tcp6://", "", 1)
-	} else if strings.HasPrefix(rserver, "ssl://") {
-		rserver = strings.Replace(rserver, "ssl://", "", 1)
-		wants_ssl = true
-	} else if strings.HasPrefix(rserver, "ssl4://") {
-		tcp_mode = "tcp4"
-		rserver = strings.Replace(rserver, "ssl4://", "", 1)
-		wants_ssl = true
-	} else if strings.HasPrefix(rserver, "ssl6://") {
-		tcp_mode = "tcp4"
-		rserver = strings.Replace(rserver, "ssl6://", "", 1)
-		wants_ssl = true
-		tcp_mode = "tcp6"
-	}
-
-	if strings.Contains(rserver, "@") {
-		// [tcp(4|6)|ssl(4|6)]://user:pass@host:port ==> [0]=user:pass [1]=host:port
-		adata0 := strings.Split(rserver, "@")[0]
-		rserver = strings.Split(rserver, "@")[1]
-		authdata := strings.Split(adata0, ":")
-
-		if len(authdata) == 2 {
-			user = authdata[0]
-			pass = authdata[1]
-			if user != "" && pass != "" {
-				wants_auth = true
-			}
-
-		} else {
-			fmt.Print("ERROR connect rserver authdata format\n")
-			os.Exit(1)
-		}
-	}
 
 	var conn net.Conn
 	var err error
-	if !wants_ssl {
-		conn, err = net.Dial(tcp_mode, rserver)
+
+	if !provider.SSL {
+		conn, err = net.Dial(provider.TCPMode, c.rserver)
 	} else {
 		conf := &tls.Config{}
-		if insecure {
+		if provider.SkipSslCheck {
 			conf.InsecureSkipVerify = true
 		}
-		conn, err = tls.Dial(tcp_mode, rserver, conf)
+		conn, err = tls.Dial(provider.TCPMode, c.rserver, conf)
 	}
 
 	if err != nil || conn == nil {
-		log.Printf("error Dial rserver=%s wants_ssl=%t err='%v' retry in 3s", rserver, wants_ssl, err)
+		log.Printf("error Dial rserver=%s wants_ssl=%t err='%v' retry in 3s", c.rserver, provider.SSL, err)
 		time.Sleep(3 * time.Second)
 		retry++
-		return c.connect(wid, rserver, insecure, provider, retry)
+		return c.connect(wid, provider, retry)
 	}
 
 	srvtp := textproto.NewConn(conn)
@@ -155,49 +105,49 @@ func (c *ProviderConns) connect(wid int, rserver string, insecure bool, provider
 		log.Printf("ERROR connect code=%d msg='%s' err='%v' retry in 3s", code, msg, err)
 		time.Sleep(3 * time.Second)
 		retry++
-		return c.connect(wid, rserver, insecure, provider, retry)
+		return c.connect(wid, provider, retry)
 	}
 
-	switch wants_auth {
+	switch c.wants_auth {
 
 	case false:
 		return &ConnItem{srvtp: srvtp, conn: conn, writer: bufio.NewWriter(conn)}, err
 
 	case true:
-		id, err := srvtp.Cmd("AUTHINFO USER %s", user)
+		id, err := srvtp.Cmd("AUTHINFO USER %s", provider.Username)
 		if err != nil {
-			log.Printf("rserver=%s AUTH1 FAILED err='%v' retry in 3s", rserver, err)
+			log.Printf("rserver=%s AUTH1 FAILED err='%v' retry in 3s", c.rserver, err)
 			time.Sleep(3 * time.Second)
 			retry++
-			return c.connect(wid, rserver, insecure, provider, retry)
+			return c.connect(wid, provider, retry)
 		}
 		srvtp.StartResponse(id)
 		code, _, err = srvtp.ReadCodeLine(381)
 		srvtp.EndResponse(id)
 		if err != nil || code != 381 {
 
-			log.Printf("rserver=%s AUTH2 FAILED code != 381 err='%v' retry in 3s", rserver, err)
+			log.Printf("rserver=%s AUTH2 FAILED code != 381 err='%v' retry in 3s", c.rserver, err)
 			time.Sleep(3 * time.Second)
 			retry++
-			return c.connect(wid, rserver, insecure, provider, retry)
+			return c.connect(wid, provider, retry)
 
 		} else {
 
-			id, err := srvtp.Cmd("AUTHINFO PASS %s", pass)
+			id, err := srvtp.Cmd("AUTHINFO PASS %s", provider.Password)
 			if err != nil {
-				log.Printf("rserver=%s AUTH3 FAILED err='%v' retry in 3s", rserver, err)
+				log.Printf("rserver=%s AUTH3 FAILED err='%v' retry in 3s", c.rserver, err)
 				time.Sleep(3 * time.Second)
 				retry++
-				return c.connect(wid, rserver, insecure, provider, retry)
+				return c.connect(wid, provider, retry)
 			}
 			srvtp.StartResponse(id)
 			code, _, err = srvtp.ReadCodeLine(281)
 			srvtp.EndResponse(id)
 			if err != nil {
-				log.Printf("rserver=%s AUTH4 FAILED err='%v' retry in 3s", rserver, err)
+				log.Printf("rserver=%s AUTH4 FAILED err='%v' retry in 3s", c.rserver, err)
 				time.Sleep(3 * time.Second)
 				retry++
-				return c.connect(wid, rserver, insecure, provider, retry)
+				return c.connect(wid, provider, retry)
 			}
 		}
 	} // end switch wants_auth
@@ -213,7 +163,7 @@ func (c *ProviderConns) GetConn(wid int, provider *Provider) (connitem *ConnItem
 		}
 		c.mux.Unlock()
 
-		connitem, err = c.connect(wid, c.rserver, c.insecure, provider, 0)
+		connitem, err = c.connect(wid, provider, 0)
 		if err != nil {
 			c.mux.Lock()
 			c.openConns--
