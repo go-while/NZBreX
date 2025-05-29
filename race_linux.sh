b1bda126c1517759f0eed043be4a13b3ff1a8d51
go-while <go-while@users.noreply.github.com>
2025-05-29 19:10:22 +0100

    Create race_linux.sh

---
+++ race_linux.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+nzbfile=/nzbs/debian-11.6.0-amd64-netinst.iso.nzb
+test "$1" != "" && nzbfile="$1"
+#./scripts/code/newBuiltNo.sh .builtno.counter
+
+# to run -race on linux: exclude the signals_windows with grep -v
+
+go run -race $(ls *.go|grep -v signals_windows) \
+        -checkfirst=false -checkonly=false \
+        -nzb "$nzbfile" -provider=provider.json \
+	-cd /cache/nzbrex -cc=true -debugcache=false \
+	-debug=false -log=false -verbose=true \
+	-crc32=false -yencout=false -yencmerge=false -yencdelparts=false \
+        -cleanhdrfile=cleanHeaders.txt \
+        -prof=false
+
