86c5f7569ed161599d5db21da7d41ee3c65bc95a
go-while <go-while@users.noreply.github.com>
2025-05-29 16:01:26 +0100

    Update race.sh

--- race.sh
+++ race.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 nzbfile=/nzbs/debian-11.6.0-amd64-netinst.iso.nzb
 test "$1" != "" && nzbfile="$1"
-#./newBuiltNo.sh .builtno.counter
-go run -race main.go Cache.go Config.go ConnPool.go Counter_uint64.go Flags.go MemLimit.go NetConn.go Results.go Routines.go Workers.go Utils.go Session.go Yenc.go \
-	-cc=true -cd /cache/nzbrex -nzb "$nzbfile" -verbose -checkfirst=false -checkonly=true -debugcache=false -prof=false -log=false -provider=provider.json -cleanhdrfile=cleanHeaders.txt -debug=false -crc32=false # -yencout=true -yencmerge=true -yencdelparts=true 
+#./scripts/code/newBuiltNo.sh .builtno.counter
+go run -race $(ls *.go|grep -v signals_windows) \
+	-cc=true -cd /cache/nzbrex -nzb "$nzbfile" -verbose -checkfirst=false -checkonly=true -debugcache=false -prof=false -log=false -provider=provider.json -cleanhdrfile=cleanHeaders.txt -debug=false -crc32=false # -yencout=true -yencmerge=true -yencdelparts=true
