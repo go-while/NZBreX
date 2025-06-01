56b58711f07e69ebe525f0a3d6cf6b68b8a097a1
go-while <go-while@users.noreply.github.com>
2025-06-01 13:35:44 +0100

    test race_linux

--- race_linux.sh
+++ race_linux.sh
@@ -6,12 +6,12 @@ test "$1" != "" && nzbfile="$1"
 # to run -race on linux: exclude the signals_windows with grep -v
 
 go run -race $(ls *.go|grep -v signals_windows) \
-	-chansize=16384 \
+	-chansize=500 \
         -checkfirst=true -checkonly=false \
         -nzb="$nzbfile" -provider=provider.json \
-	-cc=true -debugcache=false \
+	-cd=/cache/nzbrex -cc=true -debugcache=false \
 	-debug=false -debugcache=false -debugsharedcc=false -debugconnpool=false -debugworker=false -debugBUG=false \
-	-log=false -verbose=true -print430=true \
+	-log=false -verbose=true -print430=false \
 	-crc32=false -yencout=false -yencmerge=false -yencdelparts=false \
         -cleanhdrfile=cleanHeaders.txt \
         -prof=false
