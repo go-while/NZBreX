63a41e1943e53da37e626d1f920377595354fa65
go-while <go-while@users.noreply.github.com>
2025-06-06 18:25:00 +0100

    Update build_rapidyenc.sh

--- rapidyenc/build_rapidyenc.sh
+++ rapidyenc/build_rapidyenc.sh
@@ -4,3 +4,5 @@ cd build || exit 3
 cmake .. || exit 4
 cmake --build . --config Release || exit 5
 ls . rapidyenc_static/
+cd ../../ || exit 6
+cp -v rapidyenc/build/rapidyenc_static/librapidyenc.a . || exit 7
