aebe590f90c1710a8e0f42c9d61fd862becbe606
go-while <go-while@users.noreply.github.com>
2025-06-06 14:31:56 +0100

    Update rapidyenc_YES.go

--- rapidyenc_YES.go
+++ rapidyenc_YES.go
@@ -6,7 +6,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/go-while/rapidyenc" //"github.com/mnightingale/rapidyenc"
+	"github.com/go-while/NZBreX/rapidyenc" //"github.com/mnightingale/rapidyenc"
 )
 
 // compiler flags
