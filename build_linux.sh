54b9e8f69d51d24fd8c6bcd9cb64994bb1396b7c
go-while <go-while@users.noreply.github.com>
2025-06-06 20:13:45 +0100

    try linux-amd64 only on self-hosted runners

--- build_linux.sh
+++ build_linux.sh
@@ -1,2 +1,5 @@
+rm -rf rapidyenc/rapidyenc/build
+mkdir -p rapidyenc/rapidyenc/build
+cd rapidyenc && ./build_rapidyenc_linux.sh && cd ../
 go build -o NZBreX -tags other .
 exit $?
--- build_win.sh
+++ build_win.sh
@@ -1,4 +1,8 @@
+rm -rf rapidyenc/rapidyenc/build
+mkdir -p rapidyenc/rapidyenc/build
+cd rapidyenc && ./crossbuild_rapidyenc_win.sh && cd ../
+
 export GOOS=windows
 export GOARCH=amd64
-go build -o NZBreX.exe -tags windows .
+go build -o NZBreX_ry.exe -tags windows .
 exit $?
