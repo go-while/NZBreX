4ce048174297b6b366745dd18426c644cb38be2b
go-while <go-while@users.noreply.github.com>
2025-06-06 11:44:03 +0100

    Update rapidyenc_NO.go

--- rapidyenc_NO.go
+++ rapidyenc_NO.go
@@ -2,4 +2,19 @@
 
 package main
 
+import (
+	"fmt"
+)
+
 // placeholder to not compile with rapidyenc
+
+type Decoder struct{}
+type Encoder struct{}
+
+func NewDecoder() (*Decoder, error) {
+	return nil, fmt.Errorf("error: NewDecoder rapidYenc is not enabled in this build. Please enable it by building with the 'rapidyenc' tag")
+}
+
+func NewEncoder() (*Encoder, error) {
+	return nil, fmt.Errorf("error: NewEncoder rapidYenc is not enabled in this build. Please enable it by building with the 'rapidyenc' tag")
+}
