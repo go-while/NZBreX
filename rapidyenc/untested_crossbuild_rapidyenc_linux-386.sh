4471b5405ad964dad63bfe5c662fafc89e0fdcaa
go-while <go-while@users.noreply.github.com>
2025-06-07 15:30:36 +0100

    rename to match os-arch

--- rapidyenc/untested_crossbuild_rapidyenc_linux-386.sh
+++ rapidyenc/untested_crossbuild_rapidyenc_linux-386.sh
@@ -1,6 +1,6 @@
 cd rapidyenc || exit 2
 rm -rf rapidyenc/build
-mkdir -p build 
+mkdir -p build
 cd build || exit 3
 cmake .. -DCMAKE_TOOLCHAIN_FILE=../../toolchain-linux-386.cmake
 #cmake .. -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_SYSTEM_PROCESSOR=x86 \
@@ -10,4 +10,3 @@ cmake --build . --config Release || exit 5
 ls . rapidyenc_static/
 cd ../../ || exit 6
 cp -v rapidyenc/build/rapidyenc_static/librapidyenc.a . || exit 7
-cp -v rapidyenc/build/librapidyenc.dll . || exit 8
