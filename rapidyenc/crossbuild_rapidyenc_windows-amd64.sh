e26f11715738d991b1cb6b1c958b569b89005e7f
go-while <go-while@users.noreply.github.com>
2025-06-08 14:31:51 +0100

    shebang

--- rapidyenc/crossbuild_rapidyenc_windows-amd64.sh
+++ rapidyenc/crossbuild_rapidyenc_windows-amd64.sh
@@ -1,6 +1,7 @@
+#!/usr/bin/env bash
 cd rapidyenc || exit 2
-rm -rf rapidyenc/build
-mkdir -p build 
+rm -rf build
+mkdir -p build
 cd build || exit 3
 cmake .. -DCMAKE_TOOLCHAIN_FILE=../../toolchain-mingw64.cmake || exit 4
 cmake --build . --config Release || exit 5
@@ -8,3 +9,4 @@ ls . rapidyenc_static/
 cd ../../ || exit 6
 cp -v rapidyenc/build/rapidyenc_static/librapidyenc.a . || exit 7
 cp -v rapidyenc/build/librapidyenc.dll . || exit 8
+rm -rf rapidyenc/build
