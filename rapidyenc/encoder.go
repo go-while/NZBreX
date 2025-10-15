50da338bee033e7ee1532c1e081628ab26cc4d0a
go-while <go-while@users.noreply.github.com>
2025-10-15 13:01:52 +0000

    test build

--- rapidyenc/encoder.go
+++ rapidyenc/encoder.go
@@ -3,7 +3,7 @@ package rapidyenc
 /*
 #cgo CFLAGS: -I${SRCDIR}/src
 #cgo darwin LDFLAGS: ${SRCDIR}/librapidyenc_darwin.a -lstdc++
-#cgo windows,amd64 LDFLAGS: ${SRCDIR}/librapidyenc_windows_amd64.a -static-libstdc++ -static-libgcc
+#cgo windows,amd64 LDFLAGS: ${SRCDIR}/librapidyenc_windows_amd64.a -lstdc++ -static-libstdc++ -static-libgcc
 #cgo linux,amd64 LDFLAGS: ${SRCDIR}/librapidyenc_linux_amd64.a -lstdc++
 #cgo linux,arm64 LDFLAGS: ${SRCDIR}/librapidyenc_linux_arm64.a -lstdc++
 #include "rapidyenc.h"
