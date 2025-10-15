98b1c6f4dd228da849bac4f5b0ee39a6d9b17d08
go-while <go-while@users.noreply.github.com>
2025-10-15 13:24:00 +0000

    retry mac build

--- rapidyenc/build_rapidyenc_linux-amd64.sh
+++ rapidyenc/build_rapidyenc_linux-amd64.sh
@@ -7,6 +7,11 @@ cmake .. || exit 4
 cmake --build . --config Release || exit 5
 ls . rapidyenc_static/
 cd ../../ || exit 6
-cp -v rapidyenc/build/rapidyenc_static/librapidyenc.a ./librapidyenc_linux_amd64.a || exit 7
-ln -sfv ./librapidyenc_linux_amd64.a ./librapidyenc.a
+if [ "$1" = "darwin" ]; then
+ cp -v rapidyenc/build/rapidyenc_static/librapidyenc.a ./librapidyenc_darwin_amd64.a || exit 7
+ ln -sfv ./librapidyenc_darwin_amd64.a ./librapidyenc.a
+else
+ cp -v rapidyenc/build/rapidyenc_static/librapidyenc.a ./librapidyenc_linux_amd64.a || exit 7
+ ln -sfv ./librapidyenc_linux_amd64.a ./librapidyenc.a
+fi
 #rm -rf rapidyenc/build
--- rapidyenc/build_rapidyenc_linux-arm64.sh
+++ rapidyenc/build_rapidyenc_linux-arm64.sh
@@ -7,5 +7,11 @@ cmake .. -DCMAKE_TOOLCHAIN_FILE=../toolchain-linux-arm64.cmake || exit 4
 cmake --build . --config Release || exit 5
 ls . rapidyenc_static/
 cd ../../ || exit 6
-cp -v rapidyenc/build/rapidyenc_static/librapidyenc.a ./librapidyenc_linux_arm64.a || exit 7
+if [ "$1" = "darwin" ]; then
+ cp -v rapidyenc/build/rapidyenc_static/librapidyenc.a ./librapidyenc_darwin_arm64.a || exit 7
+ ln -sfv ./librapidyenc_darwin_arm64.a ./librapidyenc.a
+else
+ cp -v rapidyenc/build/rapidyenc_static/librapidyenc.a ./librapidyenc_linux_arm64.a || exit 7
+ ln -sfv ./librapidyenc_linux_arm64.a ./librapidyenc.a
+fi
 #rm -rf rapidyenc/build
