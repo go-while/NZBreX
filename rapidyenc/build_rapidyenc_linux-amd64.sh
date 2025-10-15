6090a5d30e681875ce2a4205da9c1f612f361224
go-while <go-while@users.noreply.github.com>
2025-10-15 14:09:53 +0000

    try fix filenames

--- rapidyenc/build_rapidyenc_linux-amd64.sh
+++ rapidyenc/build_rapidyenc_linux-amd64.sh
@@ -9,8 +9,8 @@ cmake --build . --config Release || exit 5
 ls . rapidyenc_static/
 cd ../../ || exit 6
 if [ "$1" = "darwin" ]; then
- cp -v rapidyenc/build/rapidyenc_static/librapidyenc.a ./librapidyenc_darwin_amd64.a || exit 7
- ln -sfv ./librapidyenc_darwin_amd64.a ./librapidyenc.a
+ cp -v rapidyenc/build/rapidyenc_static/librapidyenc.a ./librapidyenc_darwin.a || exit 7
+ ln -sfv ./librapidyenc_darwin.a ./librapidyenc.a
 else
  cp -v rapidyenc/build/rapidyenc_static/librapidyenc.a ./librapidyenc_linux_amd64.a || exit 7
  ln -sfv ./librapidyenc_linux_amd64.a ./librapidyenc.a
--- rapidyenc/build_rapidyenc_linux-arm64.sh
+++ rapidyenc/build_rapidyenc_linux-arm64.sh
@@ -9,8 +9,8 @@ cmake --build . --config Release || exit 5
 ls . rapidyenc_static/
 cd ../../ || exit 6
 if [ "$1" = "darwin" ]; then
- cp -v rapidyenc/build/rapidyenc_static/librapidyenc.a ./librapidyenc_darwin_arm64.a || exit 7
- ln -sfv ./librapidyenc_darwin_arm64.a ./librapidyenc.a
+ cp -v rapidyenc/build/rapidyenc_static/librapidyenc.a ./librapidyenc_darwin.a || exit 7
+ ln -sfv ./librapidyenc_darwin.a ./librapidyenc.a
 else
  cp -v rapidyenc/build/rapidyenc_static/librapidyenc.a ./librapidyenc_linux_arm64.a || exit 7
  ln -sfv ./librapidyenc_linux_arm64.a ./librapidyenc.a
